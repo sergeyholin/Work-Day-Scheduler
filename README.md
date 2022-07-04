5d1a75eb3416e0c9df651228b4ffaa8ffafef633
Sergey Holin <holin_sergey@yahoo.com>
2022-07-04 13:02:09 -0700

    Final commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Work-Day-Scheduler
-Module 5 Challenge
+Module 5 Challenge: Work Day Scheduler by Sergey Holin
+
+GitHub Repo Link: https://github.com/sergeyholin/Work-Day-Scheduler
+
+Live Application Link: https://sergeyholin.github.io/Work-Day-Scheduler/
+
+NOTES: If you check the app any time after 17:00, all timeblocks will be grey, because the app will see them as past events for the day. If you check the app before 09:00, all time blocks will be green, because the app will see them as future events for the day.
+
+ABOUT THE APP:
+
+Work Day Scheduler is a simple calendar application that allows a user to save events for each hour of the day.
+
+ACCEPTANCE CRITERIA:
+
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+ADDITIONAL CRITERIA:
+
+Application deployed at live URL.
+
+SCREENSHOT:
+
+<img src="./assets/img/screenshot.png/"/>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,173 +1,271 @@
+// Time--------------------------------------------------------------------------------------------
 var today = moment();
+// This code posts current time on my HTML page.
 $("#1a").text(today.format("MMM Do, YYYY HH:mm"));
-// Time-------------------------------------------------------
-
-// Saving events---------------------------------------------
-var submitButton1 = document.querySelector("#submit-button1");
-var userInput = document.querySelector("#user-input");
-var event1 = document.querySelector("#event1");
-
-// var userInput = document.querySelector("#user-input2");
-// var userInput = document.querySelector("#user-input3");
-// var userInput = document.querySelector("#user-input4");
-// var userInput = document.querySelector("#user-input5");
-// var userInput = document.querySelector("#user-input6");
-// var userInput = document.querySelector("#user-input7");
-// var userInput = document.querySelector("#user-input8");
-// var userInput = document.querySelector("#user-input9");
-// "#user-input2","#user-input3","#user-input4","#user-input5","#user-input6","#user-input7","#user-input8","#user-input9"
-
-// submitButton1.addEventListener("click", function(event) {
-//   event.preventDefault();
-//   var input = document.querySelector("#user-input").value;
-//   localStorage.setItem("input", input);
-//   renderLastInput();
-// });
-
-function test () {
-  var input = document.querySelector("#user-input").value;
-  localStorage.setItem("input", input);
-  renderLastInput();
-};
-
-function renderLastInput() {
-  var input = localStorage.getItem("input");
-  event1.textContent = input;
-};
-renderLastInput();
-
-// Saving Event done----------------------------------------------
-
-// Moment functions----------------------------------------------
+// Moment functions--------------------------------------------------------------------------------
 var format = 'HH:mm'
 // var time is current time
 var time = moment();
-
-// beforeTime = moment('20:00', format),
-// afterTime = moment('22:00', format);
-// if (time.isBetween(beforeTime, afterTime)) {
-// console.log('is between')
-// alert("time")
-// document.getElementById("test").style.backgroundColor = "red"
-// } else {
-// console.log('is not between')
-// };
-
-// Timeblock 1------------------------------------------------------
+// Timeblock 1-----------------------------------------------------------------------------------
+// Function to color code my timeblocks in relation to time: grey(#A9A9A9) = past, red(#CD5C5C) = present, green(#32CD32) = future.
 function one () {
 beforeTime = moment('09:00', format),
 afterTime = moment('10:00', format);
 if (time.isBetween(beforeTime, afterTime)) {
-document.getElementById("one").style.backgroundColor = "green"
+document.getElementById("one").style.backgroundColor = "#CD5C5C"
 } else if (time.isBefore(beforeTime)) {
-document.getElementById("one").style.backgroundColor = "blue";
+document.getElementById("one").style.backgroundColor = "#32CD32";
 } else {
-  document.getElementById("one").style.backgroundColor = "red" 
+  document.getElementById("one").style.backgroundColor = "#A9A9A9" 
 }
 };
+// Activating color-coding time function on page load.
 one();
-// Timeblock 2------------------------------------------------------
+// Function to store input in local storage.
+function userInput () {
+  var input = document.querySelector("#user-input").value;
+  localStorage.setItem("input", input);
+  renderLastInput();
+};
+// Function to render input from local storage.
+function renderLastInput() {
+  var event1 = document.querySelector("#event1");
+  var input = localStorage.getItem("input");
+  event1.textContent = input;
+};
+// Activating function to render input from local storage on page load.
+renderLastInput();
+// Timeblock 2-----------------------------------------------------------------------------------
+// Function to color code my timeblocks in relation to time: grey(#A9A9A9) = past, red(#CD5C5C) = present, green(#32CD32) = future.
 function two () {
 beforeTime = moment('10:00', format),
 afterTime = moment('11:00', format);
 if (time.isBetween(beforeTime, afterTime)) {
-document.getElementById("two").style.backgroundColor = "green"
+document.getElementById("two").style.backgroundColor = "#CD5C5C"
 } else if (time.isBefore(beforeTime)) {
-document.getElementById("two").style.backgroundColor = "blue";
+document.getElementById("two").style.backgroundColor = "#32CD32";
 } else {
-document.getElementById("two").style.backgroundColor = "red" 
+document.getElementById("two").style.backgroundColor = "#A9A9A9" 
 }
 };
+// Activating color-coding time function on page load.
 two();
-// Timeblock 3------------------------------------------------------
+// Function to store input in local storage.
+function userInput2 () {
+  var input = document.querySelector("#user-input2").value;
+  localStorage.setItem("input2", input);
+  renderLastInput2();
+};
+// Function to render input from local storage.
+function renderLastInput2() {
+  var event1 = document.querySelector("#event2");
+  var input = localStorage.getItem("input2");
+  event1.textContent = input;
+};
+// Activating function to render input from local storage on page load.
+renderLastInput2();
+// Timeblock 3-----------------------------------------------------------------------------------
+// Function to color code my timeblocks in relation to time: grey(#A9A9A9) = past, red(#CD5C5C) = present, green(#32CD32) = future.
 function three () {
 beforeTime = moment('11:00', format),
 afterTime = moment('12:00', format);
 if (time.isBetween(beforeTime, afterTime)) {
-document.getElementById("three").style.backgroundColor = "green"
+document.getElementById("three").style.backgroundColor = "	#CD5C5C"
 } else if (time.isBefore(beforeTime)) {
-document.getElementById("three").style.backgroundColor = "blue";
+document.getElementById("three").style.backgroundColor = "#32CD32";
 } else {
-document.getElementById("three").style.backgroundColor = "red" 
+document.getElementById("three").style.backgroundColor = "#A9A9A9" 
 }
 };
+// Activating color-coding time function on page load.
 three();
-// Timeblock 4------------------------------------------------------
+// Function to store input in local storage.
+function userInput3 () {
+  var input = document.querySelector("#user-input3").value;
+  localStorage.setItem("input3", input);
+  renderLastInput3();
+};
+// Function to render input from local storage.
+function renderLastInput3() {
+  var event1 = document.querySelector("#event3");
+  var input = localStorage.getItem("input3");
+  event1.textContent = input;
+};
+// Activating function to render input from local storage on page load.
+renderLastInput3();
+// Timeblock 4-----------------------------------------------------------------------------------
+// Function to color code my timeblocks in relation to time: grey(#A9A9A9) = past, red(#CD5C5C) = present, green(#32CD32) = future.
 function four () {
 beforeTime = moment('12:00', format),
 afterTime = moment('13:00', format);
 if (time.isBetween(beforeTime, afterTime)) {
-document.getElementById("four").style.backgroundColor = "green"
+document.getElementById("four").style.backgroundColor = "#CD5C5C"
 } else if (time.isBefore(beforeTime)) {
-document.getElementById("four").style.backgroundColor = "blue";
+document.getElementById("four").style.backgroundColor = "#32CD32";
 } else {
-document.getElementById("four").style.backgroundColor = "red" 
+document.getElementById("four").style.backgroundColor = "#A9A9A9" 
 }
 };
+// Activating color-coding time function on page load.
 four();
-// Timeblock 5------------------------------------------------------
+// Function to store input in local storage.
+function userInput4 () {
+  var input = document.querySelector("#user-input4").value;
+  localStorage.setItem("input4", input);
+  renderLastInput4();
+};
+// Function to render input from local storage.
+function renderLastInput4 () {
+  var event1 = document.querySelector("#event4");
+  var input = localStorage.getItem("input4");
+  event1.textContent = input;
+};
+// Activating function to render input from local storage on page load.
+renderLastInput4();
+// Timeblock 5-----------------------------------------------------------------------------------
+// Function to color code my timeblocks in relation to time: grey(#A9A9A9) = past, red(#CD5C5C) = present, green(#32CD32) = future.
 function five () {
 beforeTime = moment('13:00', format),
 afterTime = moment('14:00', format);
 if (time.isBetween(beforeTime, afterTime)) {
-document.getElementById("five").style.backgroundColor = "green"
+document.getElementById("five").style.backgroundColor = "#CD5C5C"
 } else if (time.isBefore(beforeTime)) {
-document.getElementById("five").style.backgroundColor = "blue";
+document.getElementById("five").style.backgroundColor = "#32CD32";
 } else {
-document.getElementById("five").style.backgroundColor = "red" 
+document.getElementById("five").style.backgroundColor = "#A9A9A9" 
 }
 };
+// Activating color-coding time function on page load.
 five();
-// Timeblock 6------------------------------------------------------ 
+// Function to store input in local storage.
+function userInput5 () {
+  var input = document.querySelector("#user-input5").value;
+  localStorage.setItem("input5", input);
+  renderLastInput5();
+};
+// Function to render input from local storage.
+function renderLastInput5 () {
+  var event1 = document.querySelector("#event5");
+  var input = localStorage.getItem("input5");
+  event1.textContent = input;
+};
+// Activating function to render input from local storage on page load.
+renderLastInput5();
+// Timeblock 6-----------------------------------------------------------------------------------
+// Function to color code my timeblocks in relation to time: grey(#A9A9A9) = past, red(#CD5C5C) = present, green(#32CD32) = future.
 function six () {
 beforeTime = moment('14:00', format),
 afterTime = moment('15:00', format);
 if (time.isBetween(beforeTime, afterTime)) {
-document.getElementById("six").style.backgroundColor = "green"
+document.getElementById("six").style.backgroundColor = "#CD5C5C"
 } else if (time.isBefore(beforeTime)) {
-document.getElementById("six").style.backgroundColor = "blue";
+document.getElementById("six").style.backgroundColor = "#32CD32";
 } else {
-document.getElementById("six").style.backgroundColor = "red" 
+document.getElementById("six").style.backgroundColor = "#A9A9A9" 
 }
 };
+// Activating color-coding time function on page load.
 six();
-// Timeblock 7------------------------------------------------------
+// Function to store input in local storage.
+function userInput6 () {
+  var input = document.querySelector("#user-input6").value;
+  localStorage.setItem("input6", input);
+  renderLastInput6();
+};
+// Function to render input from local storage.
+function renderLastInput6 () {
+  var event1 = document.querySelector("#event6");
+  var input = localStorage.getItem("input6");
+  event1.textContent = input;
+};
+// Activating function to render input from local storage on page load.
+renderLastInput6();
+// Timeblock 7-----------------------------------------------------------------------------------
+// Function to color code my timeblocks in relation to time: grey(#A9A9A9) = past, red(#CD5C5C) = present, green(#32CD32) = future.
 function seven () {
 beforeTime = moment('15:00', format),
 afterTime = moment('16:00', format);
 if (time.isBetween(beforeTime, afterTime)) {
-document.getElementById("seven").style.backgroundColor = "green"
+document.getElementById("seven").style.backgroundColor = "#CD5C5C"
 } else if (time.isBefore(beforeTime)) {
-document.getElementById("seven").style.backgroundColor = "blue";
+document.getElementById("seven").style.backgroundColor = "#32CD32";
 } else {
-document.getElementById("seven").style.backgroundColor = "red" 
+document.getElementById("seven").style.backgroundColor = "#A9A9A9" 
 }
 };
+// Activating color-coding time function on page load.
 seven();
-// Timeblock 8------------------------------------------------------
+// Function to store input in local storage.
+function userInput7 () {
+  var input = document.querySelector("#user-input7").value;
+  localStorage.setItem("input7", input);
+  renderLastInput7();
+};
+// Function to render input from local storage.
+function renderLastInput7() {
+  var event1 = document.querySelector("#event7");
+  var input = localStorage.getItem("input7");
+  event1.textContent = input;
+};
+// Activating function to render input from local storage on page load.
+renderLastInput7();
+// Timeblock 8-----------------------------------------------------------------------------------
+// Function to color code my timeblocks in relation to time: grey(#A9A9A9) = past, red(#CD5C5C) = present, green(#32CD32) = future.
 function eight () {
 beforeTime = moment('16:00', format),
 afterTime = moment('17:00', format);
 if (time.isBetween(beforeTime, afterTime)) {
-document.getElementById("eight").style.backgroundColor = "green"
+document.getElementById("eight").style.backgroundColor = "#CD5C5C"
 } else if (time.isBefore(beforeTime)) {
-document.getElementById("eight").style.backgroundColor = "blue";
+document.getElementById("eight").style.backgroundColor = "#32CD32";
 } else {
-document.getElementById("eight").style.backgroundColor = "red"
+document.getElementById("eight").style.backgroundColor = "#A9A9A9"
 }
 };
+// Activating color-coding time function on page load.
 eight();
-// Timeblock 9------------------------------------------------------
+// Function to store input in local storage.
+function userInput8 () {
+  var input = document.querySelector("#user-input8").value;
+  localStorage.setItem("input8", input);
+  renderLastInput8();
+};
+// Function to render input from local storage.
+function renderLastInput8 () {
+  var event1 = document.querySelector("#event8");
+  var input = localStorage.getItem("input8");
+  event1.textContent = input;
+};
+// Activating function to render input from local storage on page load.
+renderLastInput8();
+// Timeblock 9-----------------------------------------------------------------------------------
+// Function to color code my timeblocks in relation to time: grey(#A9A9A9) = past, red(#CD5C5C) = present, green(#32CD32) = future.
 function nine () {
 beforeTime = moment('17:00', format),
 afterTime = moment('18:00', format);
 if (time.isBetween(beforeTime, afterTime)) {
-document.getElementById("nine").style.backgroundColor = "green"
+document.getElementById("nine").style.backgroundColor = "#CD5C5C"
 } else if (time.isBefore(beforeTime)) {
-document.getElementById("nine").style.backgroundColor = "blue";
+document.getElementById("nine").style.backgroundColor = "#32CD32";
 } else {
-document.getElementById("nine").style.backgroundColor = "red" 
+document.getElementById("nine").style.backgroundColor = "#A9A9A9" 
 }
 };
+// Activating color-coding time function on page load.
 nine();
+// Function to store input in local storage.
+function userInput9 () {
+  var input = document.querySelector("#user-input9").value;
+  localStorage.setItem("input9", input);
+  renderLastInput9();
+};
+// Function to render input from local storage.
+function renderLastInput9 () {
+  var event1 = document.querySelector("#event9");
+  var input = localStorage.getItem("input9");
+  event1.textContent = input;
+};
+// Activating function to render input from local storage on page load.
+renderLastInput9();
+// End---------------------------------------------------------------------------------------------
 
